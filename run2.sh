830341e886cc039358ad50df20b496ebbb9eb5c1
Lennart van Gijtenbeek <lgijtenbeek@desktop-40.students.os3.nl>
2018-03-13 22:25:26 +0100

    deleted

--- run2.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-sudo java -cp /sne/home/lgijtenbeek/Downloads/lucene-solr-master/lucene/core/src/java/ Application /sne/home/lgijtenbeek/Desktop/CCF_ELKPROJECT/NFS/sntgcBbhRp-wJs1YzNCgyg/0/index
